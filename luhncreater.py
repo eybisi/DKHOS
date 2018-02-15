c1a24dad7c812e78f2701bb0788231556aeba3eb
Ahmet Bilal Can <eybisii@gmail.com>
2018-02-15 18:38:17 +0300

    Create luhncreater.py

---
+++ luhncreater.py
@@ -0,0 +1,21 @@
+
+def luhn_checksum(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    return checksum % 10
+
+def is_luhn_valid(card_number):
+    return luhn_checksum(card_number) == 0
+
+
+for i in range(1000000):
+	t = 5575980000000364+i*10000
+	if(is_luhn_valid(t)):
+		print(t)
